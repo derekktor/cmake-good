70c1775968beb84f12427b1154b32c17d184e935
Derek <baldka1109bh@gmail.com>
2021-03-25 14:46:44 +0100

    created directory for adding library

---
+++ adding-lib/build/CMakeFiles/addLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/addLibrary.dir/main.cpp.o"
+  "addLibrary"
+  "addLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/addLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ adding-lib/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+#include"hello.hpp"
+
+int main() {
+    hello::say_hello();
+
+    return 0; 
+}
